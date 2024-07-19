2ba9daab4c44c7336fd8cbdbb7ca50af0f3419a9
ryanphobia <31889853+ryanphobia@users.noreply.github.com>
2024-07-19 10:48:16 +0700

    Add repair DB option

--- wp/wp-config.php
+++ wp/wp-config.php
@@ -107,6 +107,8 @@ if (isset($_ENV['S3_KEY_ID']) && isset($_ENV['S3_ACCESS_KEY'])) {
 define('DISALLOW_FILE_EDIT', true );
 define('DISALLOW_FILE_MODS', true );
 
+// Allow repair database.
+define('WP_ALLOW_REPAIR', true);
 /* That's all, stop editing! Happy publishing. */
 
 /** Absolute path to the WordPress directory. */
